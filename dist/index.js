bee9d4e253aadca346a6c5e16aa3f68c6f4fb356
devzolo <zolotorevsky@gmail.com>
2021-11-07 02:37:59 -0300

    Updates

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const addon = require(`../bin/${process.platform}/${process.arch}/native-glu`);
-exports.default = addon;
-//# sourceMappingURL=index.js.map
